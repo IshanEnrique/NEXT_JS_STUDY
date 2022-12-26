02a73a307a6cc36f28e51890d1c71887a70c5772
Rahul Kumar <rahul.kumar0850@gmail.com>
2022-12-26 14:28:54 +0530

    dev

--- hunting-coder/pages/blogpost/[slug].js
+++ hunting-coder/pages/blogpost/[slug].js
@@ -13,6 +13,7 @@ const Slug = () => {
         <hr />
         <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, illo officiis soluta voluptates iste fugiat! Quo fuga ab libero quam, magnam nulla eaque aliquid animi, similique facilis qui quibusdam laudantium.
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. At mollitia velit id repellat debitis corrupti explicabo! Quas quibusdam reiciendis facilis non eaque dolor nesciunt commodi nemo at! Adipisci, in doloribus!
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas eos labore corporis officiis dolore, voluptates minima corrupti doloremque et hic veniam nesciunt aliquid pariatur commodi saepe explicabo maiores sint atque!
         </div>
       </main>
     </div>
