860e8602989ca07f206eff8be1415eb31c120e9b
Rahul Kumar <rahul.kumar0850@gmail.com>
2022-12-28 22:29:57 +0530

    Blogs API call

--- hunting-coder/pages/blogpost/[slug].js
+++ hunting-coder/pages/blogpost/[slug].js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import blogPost from "../../styles/BlogPost.module.css";
 // Step 1 : Find the file to the slug
@@ -6,15 +6,25 @@ import blogPost from "../../styles/BlogPost.module.css";
 const Slug = () => {
   const router = useRouter();
   let { slug } = router.query;
-
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    if(!router.isReady) return;
+    fetch(`http://localhost:3000/api/getblog?slug=${slug}`)
+      .then((a) => {
+        return a.json();
+      })
+      .then((parsed) => {
+        console.log(parsed);
+        setBlogs(parsed);
+      });
+  }, [router.isReady])
   return (
     <div className={blogPost.conainer} >
       <main className={blogPost.main}>
-        <h1>Title of the page - {slug}</h1>
+        <h1>{blogs.title}</h1>
         <hr />
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, illo officiis soluta voluptates iste fugiat! Quo fuga ab libero quam, magnam nulla eaque aliquid animi, similique facilis qui quibusdam laudantium.
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. At mollitia velit id repellat debitis corrupti explicabo! Quas quibusdam reiciendis facilis non eaque dolor nesciunt commodi nemo at! Adipisci, in doloribus!
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas eos labore corporis officiis dolore, voluptates minima corrupti doloremque et hic veniam nesciunt aliquid pariatur commodi saepe explicabo maiores sint atque!
+        <div>
+          {blogs.content}
         </div>
       </main>
     </div>
