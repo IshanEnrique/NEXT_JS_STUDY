381a9851497a27aebbbaf161b90d7df0edd7c164
Rahul Kumar <rahul.kumar0850@gmail.com>
2022-12-29 15:40:31 +0530

    Dangerously set inner html for about

---
+++ hunting-coder/pages/api/getDynamicHtml.js
@@ -0,0 +1,28 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import type { NextApiRequest, NextApiResponse } from 'next'
+// http://localhost:3000/api/getblog?slug=how-to-learn-java
+import * as fs from 'fs';
+import { unescape } from 'querystring';
+
+
+
+export default  function  handler(
+  req,
+  res
+) {
+  fs.readFile(`htmls/${req.query.slug}.html`, "utf-8", (err,data) => {
+    if(err){
+      let error={
+        status:"ERR0001",
+        message:"No such html found"
+      }
+      return res.status(500).json(error);
+    }
+        
+    res.status(200).json({
+      status:"00",
+      message:"File read completed.",
+      data:data
+    });
+  });
+}
