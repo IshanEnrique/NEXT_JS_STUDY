e38c934a76bd360fb8772858cde54e4619bb7184
Rahul Kumar <rahul.kumar0850@gmail.com>
2022-12-30 13:48:12 +0530

    INITIAL COMMIT FOR CODESWEAR.COM

--- codeswear/pages/api/hello.ts
+++ codeswear/pages/api/hello.ts
@@ -1,18 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-const connectToMongo = require('../../db');
-const express = require('express');
-const router = express.Router();
-connectToMongo();
 type Data = {
   name: string
 }
-
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'This is homepage' })
+  res.status(200).json({ name: 'John Doe' })
 }
--- hunting-coder/pages/home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import HomeBlog from './components/HomeBlog'
-const home = () => {
-  return (
-    <HomeBlog />
-  )
-}
-
-export default home
