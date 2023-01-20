1c6f19e7f75b9e4d65b1ad52289ec99206d29687
Rahul Kumar <rahul.kumar0850@gmail.com>
2023-01-20 23:58:54 +0530

    toggleCart

--- codeswear/components/Navbar.js
+++ codeswear/components/Navbar.js
@@ -1,9 +1,19 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
-import { AiOutlineShoppingCart } from 'react-icons/ai'
+import React, { useRef } from 'react'
+import { AiFillCloseSquare, AiOutlineShoppingCart } from 'react-icons/ai'
 
 const Navbar = () => {
+    const toggleCart=()=>{
+        if(ref.current.classList.contains('translate-x-full')){
+            ref.current.classList.remove("translate-x-full");
+            ref.current.classList.add("translate-x-0");
+        }else{
+            ref.current.classList.add("translate-x-full");
+            ref.current.classList.remove("translate-x-0");
+        }
+    }
+    const ref = useRef();
     return (
         <div className='flex flex-col md:flex-row md:justify-start justify-center items-center shadow-md'>
             <div className="logo">
@@ -19,8 +29,18 @@ const Navbar = () => {
                     <Link legacyBehavior href={"/sticker"}><a><li>Stickers</li></a></Link>
                 </ul>
             </div>
-            <div className="cart absolute right-0 top-2 mx-5">
+            <div onClick={toggleCart} className="cart absolute right-0 top-2 mx-5 cursor-pointer">
                 <AiOutlineShoppingCart className='text-xl md:text-3xl' />
+            </div>
+
+            <div ref={ref} className="sideCart absolute top-0 right-0 bg-gray-100 p-10 transform transition-transform translate-x-full">
+                <h2 className="font-bold text-xl">Shopping Cart</h2>
+                <span onClick={toggleCart} className="absolute top-2 right-2 cursor-pointer text-2xl text-gray-500"><AiFillCloseSquare  /></span>
+                <ol>
+                    <li>
+                        <span>T-Shirt - Wear The Code</span>
+                    </li>
+                </ol>
             </div>
         </div>
     )
