87cb470aaaaf77280dd7c3e88f52409d786dcaf9
Rahul Kumar <rahul.kumar0850@gmail.com>
2023-02-17 19:55:03 +0530

    test

--- codeswear/pages/api/getAllTShirts.js
+++ codeswear/pages/api/getAllTShirts.js
@@ -1,21 +1,31 @@
-export default function handler(
+
+
+  
+
+ 
+
+
+export default async function handler(
     req,
     res
   ) {
-    let tshirt={
-        href:"/product/wear-the-code",
-        imgUrl:"https://m.media-amazon.com/images/I/71sGfOZcpwL._UY879_.jpg",
-        price:499,
-        productCode:"t-shirt-0001",
-        name: "T-Shirts",
-        size: ["S", "M", "L", "XL", "XXL"],
-        vairant: ["Black"]
-    }
-
-    let allTshirts=[];
-    for(let i=0;i<20;i++){
+    
+      
+      
+      let allTshirts=[];
+      for(let i=0;i<20;i++){
+        let tshirt={
+          href:"/product/wear-the-code",
+          imgUrl:"https://m.media-amazon.com/images/I/71sGfOZcpwL._UY879_.jpg",
+          price:499,
+          productCode:"T-Shirt-",
+          name: "T-Shirts",
+          size: ["S", "M", "L", "XL", "XXL"],
+          vairant: ["Black"]
+        }
+        tshirt.productCode=tshirt.productCode+i;
         allTshirts.push(tshirt);
-    }
+      }
     res.status(200).json(allTshirts)
   }
   
--- codeswear/pages/tshirts.js
+++ codeswear/pages/tshirts.js
@@ -3,55 +3,61 @@ import React, { useEffect, useState } from 'react'
 
 const Tshirts = () => {
   const [tshirts, setTshirts] = useState([]);
-    
-    const getTShirts = async () => {
-        let tShirtsRes = await fetch("/api/getAllTShirts")
-        if (tShirtsRes.status == 200) {
-            let tshirtsJson = await tShirtsRes.json();
-            setTshirts(tshirtsJson);
+  const [size, setSize] = useState("M");
+  const onChangeSize=(e)=>{
+    setSize(e.target.value);
+  }
+  const getTShirts = async () => {
+    let tShirtsRes = await fetch("/api/getAllTShirts")
+    if (tShirtsRes.status == 200) {
+      let tshirtsJson = await tShirtsRes.json();
+      setTshirts(tshirtsJson);
 
-        } else {
-            console.info(">>>>>>>>Get TShirts API Error response  : " + tShirtsRes);
+    } else {
+      console.info(">>>>>>>>Get TShirts API Error response  : " + JSON.stringify(tShirtsRes));
 
-        }
     }
+  }
 
-    useEffect(() => {
-      getTShirts();  
-    }, [])
+  useEffect(() => {
+    getTShirts();
+  }, [])
+
+  const productDetails=(product)=>{
     
+  }
   return (
     <div>
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4">
             {
-              tshirts.map((tshirt)=>{
+              tshirts.map((tshirt,index) => {
 
                 return (
-                <Link key={tshirt.productCode} href={tshirt.href} legacyBehavior><a><div className="lg:w-full md:w-full p-4 w-full shadow-lg m-5">
-                  <a className="block relative h-48 rounded overflow-hidden">
-                    <img alt="ecommerce" className="h-[50vh] block" src={tshirt.imgUrl} />
-                  </a>
-                  <div className="mt-4 text-center">
-                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{tshirt.name+"-"+tshirt.vairant[0]}</h3>
-                    <h2 className="text-gray-900 title-font text-lg font-medium">Wear The Code</h2>
-                    <p className="mt-1">₹{tshirt.price}</p>
-                    <p className="mt-1" >SIZE :{tshirt.size.map((element) => {
-                   return ( 
-                  //    {element} </p>
-                <span key={element} className="mt-1 mx-2" >
-                  {element}
-                  <input className="mt-2 pt-1 mx-1"   type="checkbox" name="size" id="size" checked={false} value={element} />
-                </span> 
-                    )
-                      
-                    })}
-                    </p>
+                  <Link key={tshirt.productCode} href="#" onClick={productDetails(tshirt)} legacyBehavior><a><div className="lg:w-full md:w-full p-4 w-full shadow-lg m-5">
+                    <a className="block relative h-48 rounded overflow-hidden">
+                      <img alt="ecommerce" className="h-[50vh] block" src={tshirt.imgUrl} />
+                    </a>
+                    <div className="mt-4 text-center">
+                      <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{tshirt.name + "-" + tshirt.vairant[0]}</h3>
+                      <h2 className="text-gray-900 title-font text-lg font-medium ">{size}</h2>
+                      <p className="mt-1">₹{tshirt.price}</p>
+                      <p className="mt-1" >SIZE :{tshirt.size.map((element) => {
+                        return (
+                          
+                          <span key={element} className="mt-1 mx-2" >
+                            {element}
+                            <input className="mt-2 pt-1 mx-1" type="checkbox" name="size" id="size" checked={false} value={element} onChange={onChangeSize} />
+                          </span>
+                        )
+
+                      })}
+                      </p>
+                    </div>
                   </div>
-                </div>
-                </a>
-                </Link>
+                  </a>
+                  </Link>
                 )
               })
 
